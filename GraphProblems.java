3a6bee6d8e78f945902c6a65e4e9991943a47943
mxhit17 <limmudgal@gmail.com>
2023-09-09 23:06:30 +0530

    Upto Part2

--- GraphProblems.java
+++ GraphProblems.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class GraphProblems {
     static class Edge{
@@ -12,40 +13,121 @@ public class GraphProblems {
             this.wt = w;
         }
     }
-    public static void main(String[] args) {
-        int V = 5;
-        ArrayList<Edge>[] graph = new ArrayList[V]; // null -> empty ArrayList
 
+    static void createGraph(ArrayList<Edge> graph[]){
         for(int i = 0; i < graph.length; i++){
             graph[i] = new ArrayList<>();
         }
 
-        // 0 - vertex
-        graph[0].add(new Edge(0, 1, 5));
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
 
-        // 1 - vertex
-        graph[1].add(new Edge(1, 0, 5));
-        graph[1].add(new Edge(1, 2, 1));
-        graph[1].add(new Edge(1, 3, 3));
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
 
-        // 2 - vertex
-        graph[2].add(new Edge(2, 1, 1));
-        graph[2].add(new Edge(2, 3, 1));
-        graph[2].add(new Edge(2, 4, 2));
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
 
-        // 3 - vertex
-        graph[3].add(new Edge(3, 1, 3));
-        graph[3].add(new Edge(3, 2, 1));
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
 
-        // 4 - vertex
-        graph[4].add(new Edge(4, 2, 2));
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
 
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
 
-        for(int i = 0; i < graph[2].size(); i++){
-            Edge e = graph[2].get(i);
-            System.out.print(e.dest + " ");
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
+    public static void bfsUtil(ArrayList<Edge>[] graph, boolean vis[]){
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.add(0);
+
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            if(!vis[curr]){
+                System.out.print(curr + " ");
+                vis[curr] = true;
+                for(int i = 0; i < graph[curr].size(); i++){
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
         }
-        System.out.println();
+    }
+
+    public static void bfs(ArrayList<Edge> graph[]){
+        boolean vis[] = new boolean[graph.length];
+        for(int i = 0; i < graph.length; i++){
+            if(!vis[i]){
+                bfsUtil(graph, vis);
+            }
+        }
+    }
+
+    public static void dfsUtil(ArrayList<Edge>[] graph, int curr, boolean visited[]){
+        System.out.print(curr + " ");
+        visited[curr] = true;
+
+        for(int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(!visited[e.dest]){
+                dfsUtil(graph, e.dest, visited);
+            }
+        }
+
+        return;
+    }
+
+    public static void dfs(ArrayList<Edge> graph[]){
+        boolean visited[] = new boolean[graph.length];
+
+        for(int i = 0; i < graph.length; i++){
+            if(!visited[i]){
+                dfsUtil(graph, i, visited);
+            }
+        }
+    }
+
+    public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean visited[]){
+        if(src == dest){
+            return true;
+        }
+
+        visited[src] = true;
+
+        for(int i = 0; i < graph[src].size(); i++){
+            Edge e = graph[src].get(i);
+            // e.dest = meighbour
+            if(!visited[e.dest] && hasPath(graph, e.dest, dest, visited)){
+                return true;
+            }
+            // visited[e.dest] = true;
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+        int V = 7;
+        ArrayList<Edge>[] graph = new ArrayList[V]; // null -> empty ArrayList
+        createGraph(graph);
+
+
+        // BFS
+        // bfsUtil(graph);
+
+
+        // DFS
+        // dfsUtil(graph, 0, visited);
+
+
+        // Has Path
+        // System.out.println(hasPath(graph, 0, 5, new boolean[V]));
 
     }
 }
