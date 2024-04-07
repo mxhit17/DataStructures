479400208fae1c53a70c4a96f6737bc35deb2a71
mxhit17 <limmudgal@gmail.com>
2024-04-07 10:14:26 +0530

    Saving

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -1,4 +1,6 @@
-import java.util.Arrays;
+// import java.util.*;
+
+import javax.swing.plaf.synth.SynthStyle;
 
 public class DynamicProgramming {
     public static int fibonacci(int n){     // purely recurrsive    TC = O(2^n)
@@ -117,8 +119,131 @@ public class DynamicProgramming {
             return dp[n][W];
         } else { // not valid
             dp[n][W] = knapSackMemo(val, wt, W, n - 1, dp);
+
             return dp[n][W];
         }
+    }
+
+    public static int knapsackTab(int val[], int wt[], int W) {
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
+        // Initialization
+        for(int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+
+        for(int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= W; j++) {
+                int v = val[i - 1];
+                int w = wt[i - 1];
+                if (w <= j) { // Valid Case
+                    // Include
+                    int incProfit = v + dp[i - 1][j - w];
+                    // Exclude
+                    int excProfit = dp[i - 1][j];
+
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                } else { // Invalid Case
+                    int excProfit = dp[i - 1][j];
+
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+
+    public static void printDP(boolean dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (dp[i][j]) {
+                    System.out.print("T" + " ");
+                } else {
+                    System.out.print("F" + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static boolean targetSum(int numbers[], int target) { // O(n * target)
+        boolean dp[][] = new boolean[numbers.length + 1][target + 1];
+
+        for (int i = 0; i < numbers.length + 1; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 0; i < target + 1; i++) {
+            dp[0][i] = false;
+        }
+
+        for (int i = 1; i < numbers.length + 1; i++) {
+            for (int j = 1; j < target + 1; j++) {
+                int v = numbers[i - 1];
+                // Include
+                if (v <= j && dp[i - 1][j - v] == true) {
+                    dp[i][j] = true;
+                } else if (dp[i - 1][j] == true) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        printDP(dp);
+
+        return dp[numbers.length][target];
+    }
+
+    public static void printDP(int dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static int unBoundedKsTab(int val[], int wt[], int W) { // O(n * W)
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
+        // Initialization
+        for(int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+
+        for(int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= W; j++) {
+                int v = val[i - 1];
+                int w = wt[i - 1];
+                if (w <= j) { // Valid Case
+                    // Include
+                    int incProfit = v + dp[i][j - w];
+                    // Exclude
+                    int excProfit = dp[i - 1][j];
+
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                } else { // Invalid Case
+                    int excProfit = dp[i - 1][j];
+
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+
+        printDP(dp);
+
+        return dp[n][W];
     }
     public static void main(String[] args) {
         // Fibonacci Series
@@ -142,9 +267,9 @@ public class DynamicProgramming {
 
         // 0/1 Knapsack
         // Recursion
-        int val[] = {15, 14, 10, 45, 30};
-        int wt[] = {2, 5, 1, 3, 4};
-        int W = 7;
+        // int val[] = {15, 14, 10, 45, 30};
+        // int wt[] = {2, 5, 1, 3, 4};
+        // int W = 7;
         // System.out.println(knapSack(val, wt, W, val.length));
 
         // Memoization
@@ -157,6 +282,19 @@ public class DynamicProgramming {
         // System.out.println(knapSackMemo(val, wt, W, val.length, dp));
 
         // Tabulation
-        
+        // System.out.println(knapsackTab(val, wt, W));
+
+
+        // Target Sum Subset
+        // int numbers[] = {4, 2, 7, 1, 3};
+        // int target = 10;
+        // System.out.println(targetSum(numbers, target));
+
+
+        // Unbounded Knapsack
+        // int val[] = {15, 14, 10, 45, 30};
+        // int wt[] = {2, 5, 1, 3, 4};
+        // int W = 7;
+        // System.out.println(unBoundedKsTab(val, wt, W));
     }
 }
--- xxPracticeArea.java
+++ xxPracticeArea.java
@@ -1,64 +1,16 @@
-import java.util.Arrays;
-import java.util.Collections;
-
 public class xxPracticeArea {
-    static int max(int a, int b) { return (a > b) ? a : b; }
-    static int knapSack(int W, int wt[], int val[], int n)
-    {
-        int i, w;
-        int K[][] = new int[n + 1][W + 1];
+    public static void main(String args[]) {
+        int dist[][] = {{0, 16, 11, 6},
+                        {8, 0, 13, 16},
+                        {4, 7, 0, 9},
+                        {5, 12, 2, 0}};
 
-
-        for (i = 0; i <= n; i++) {
-            for (w = 0; w <= W; w++) {
-                if (i == 0 || w == 0)
-                    K[i][w] = 0;
-                else if (wt[i - 1] <= w)
-                    K[i][w]
-                        = max(val[i - 1]
-                                  + K[i - 1][w - wt[i - 1]],
-                              K[i - 1][w]);
-                else
-                    K[i][w] = K[i - 1][w];
-            }
-        }
-
-        return K[n][W];
-    }
-
-    public static void main(String args[])
-    {
-        int profit[] = new int[] { 60, 100, 120 };
-        int weight[] = new int[] { 10, 20, 30 };
-        int W = 50;
-        int n = profit.length;
-        System.out.println(knapSack(W, weight, profit, n));
-    }
-}
-
-class Solution {
-    public static int minElements(int n, int m, int[] arr1, int[] arr2) {
-        // code here
-        int sum = 0;
+        int n = 4;
 
         for (int i = 0; i < n; i++) {
-            sum += arr1[i];
-        }
-
-        Arrays.sort(arr2);
-
-        int count = 0;
-        int check = 0;
-
-        for (int i = m - 1; i >= 0; i--) {
-            check += arr2[i];
-            count += 1;
-
-            if (check >= sum) {
-                return count;
+            for (int j = 0; j < n; j++) {
+                
             }
         }
-
-        return -1;
     }
 }
